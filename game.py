a3bca46103dac8902b70d506a0b0bdb76eb2b04c
angelal14 <66487575+angelal14@users.noreply.github.com>
2020-06-14 21:10:55 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ Player can walk around atrium to talk to characters & make drinks at Opus
 
 """
 import arcade
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -135,40 +136,35 @@ class MyGame(arcade.View):
         self.char5.draw()
 
         #check collision -- THERE HAS TO BE A CLEANER WAY TO DO THIS??
-        self.collide_char1 = arcade.check_for_collision(self.char5,self.char1)
-        if self.collide_char1 == True:
+        if arcade.check_for_collision(self.char5,self.char1):
             self.speech1 = arcade.Sprite("game1_images/dialogue.png",scale=0.07,
                           center_x=270,center_y=475)
             self.speech1.draw()
             arcade.draw_text("Have you\ntried Opus\ntoday?",270,450,arcade.color.BLACK,10,
                              anchor_x="center",rotation=20)
 
-        self.collide_char2 = arcade.check_for_collision(self.char5,self.char2)
-        if self.collide_char2 == True:
+        if arcade.check_for_collision(self.char5,self.char2):
             self.speech2 = arcade.Sprite("game1_images/dialogue270.png",scale=0.07,
                                          center_x=460,center_y=465)
             self.speech2.draw()
             arcade.draw_text("Aren't\nyou late\n    for\n   work??",465,440,arcade.color.BLACK,10,
                              anchor_x="center")
 
-        self.collide_char3 = arcade.check_for_collision(self.char5,self.char3)
-        if self.collide_char3 == True:
+        if arcade.check_for_collision(self.char5,self.char3):
             self.speech3 = arcade.Sprite("game1_images/dialogue90.png",scale=0.07,
                                          center_x=470,center_y=45)
             self.speech3.draw()
             arcade.draw_text("Have\nyou\ndone the\nhome-\nwork?",470,20,arcade.color.BLACK,9,
                              anchor_x="center")
 
-        self.collide_char4 = arcade.check_for_collision(self.char5, self.char4)
-        if self.collide_char4 == True:
+        if arcade.check_for_collision(self.char5, self.char4):
             self.speech4 = arcade.Sprite("game1_images/dialogue270.png", scale=0.07,
                                          center_x=580, center_y=300)
             self.speech4.draw()
-            arcade.draw_text("Have\nfun at\nwork!", 580, 300, arcade.color.BLACK, 10,
+            arcade.draw_text("Have\nfun at\nwork!", 580, 280, arcade.color.BLACK, 10,
                              anchor_x="center")
 
-        self.collide_opus = arcade.check_for_collision(self.char5,self.opus)
-        if self.collide_opus == True:
+        if arcade.check_for_collision(self.char5,self.opus):
             game_view = Instruction2View()
             self.window.show_view(game_view)
 
@@ -201,9 +197,9 @@ class MyGame(arcade.View):
             self.char5.change_y = 0
 
     #DELETE THIS LATER... JUST FOR TESTING USE
-#     def on_key_press(self, symbol, modifiers):
-#             game_view = Instruction2View()
-#             self.window.show_view(game_view)
+    def on_key_press(self, symbol, modifiers):
+            game_view = Instruction2View()
+            self.window.show_view(game_view)
 
 
 class Instruction2View(arcade.View):
@@ -242,8 +238,6 @@ class OpusView(arcade.View):
         super().__init__()
         arcade.set_background_color(arcade.color.BISQUE)
 
-        #Initialize sprite lists here, set them to None
-
         self.instructions = "Hey! Glad you could make it to work. We have\na busy day ahead of us!"
         self.press_space = "(Press SPACE to continue)"
         self.pos_x = SCREEN_WIDTH / 2
@@ -254,18 +248,48 @@ class OpusView(arcade.View):
 
         self.space_count = 0
         self.cup = None
-        #music lol
-        # self.wannabeyours = .load_sound("game1_images/wannabeyours.mp3")
-        # arcade.play_sound(self.wannabeyours)
+        self.time = 35
+
+        #stations
+        self.iced_tea = None
+        self.coffee_station = None
+        self.milk_station = None
+        self.syrup_station = None
+        self.ice_station = None
+        self.whipped_cream = None
+        self.trash = None
+
+        self.game_start = False
+        self.drink = None
+
+        #logic check
+        self.syrup_done = False
+        self.ice_done = False
+        self.coffee_done = False
+        self.complete = False
+        self.order_num = 1
+        self.cup_empty = True
+        self.failed = False
+        self.levels_completed = 0
+        self.level = 1
 
     def setup(self):
-        self.you = arcade.Sprite("game1_images/you.png", scale=0.45, center_x=720, center_y=100)
+        self.you = arcade.Sprite("game1_images/you.png", scale=0.45, center_x=500, center_y=305)
         self.boss = arcade.Sprite("game1_images/boss.png", scale=0.19, center_x=100, center_y=460)
-        self.cup = arcade.Sprite("game1_images/cup.png",scale=0.23,center_x=400,center_y=270)
+        self.cup = arcade.Sprite("game1_images/cup.png",scale=0.18,center_x=400,center_y=305)
+
+        #stations
+        self.trash = arcade.Sprite("game1_images/trash.png",scale=0.15,center_x=730,center_y=400)
+        self.coffee_station = arcade.Sprite("game1_images/coffee_station.jpg",scale=0.15,center_x=80,center_y=250)
+        self.milk_station = arcade.Sprite("game1_images/milk.png",scale=0.08,center_x=80,center_y=80)
+        self.syrup_station = arcade.Sprite("game1_images/syrup_station.png",scale=0.15,center_x=370,center_y=70)
+        self.ice_station = arcade.Sprite("game1_images/ice.jpg",scale=0.15,center_x=490,center_y=70)
+        self.whipped_cream = arcade.Sprite("game1_images/whipped_cream.png",scale=0.15,center_x=580,center_y=70)
+        self.iced_tea = arcade.Sprite("game1_images/iced_tea.png",scale=0.15,center_x=730,center_y=70)
+
 
     def on_draw(self):
         arcade.start_render()
-        self.you.draw()
         self.boss.draw()
 
         arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, 520, 600, 100, arcade.color.LIGHT_STEEL_BLUE)
@@ -273,42 +297,190 @@ class OpusView(arcade.View):
                          anchor_x="center",anchor_y="center",align="center")
         arcade.draw_text(self.press_space, 320, 480, arcade.color.BLACK, 10)
 
-    def on_update(self, delta_time):
-        pass
+        if self.space_count > 0:
+            self.cup.draw()
+        if self.game_start == True:
+            arcade.draw_text(f"Time remaining: {self.time:.0f}", 400,456,arcade.color.BLACK,anchor_x="center",
+                             anchor_y="center",align="center",font_name="Comic Sans MS",font_size=15)
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        pass
+        self.iced_tea.draw()
+        self.coffee_station.draw()
+        self.milk_station.draw()
+        self.syrup_station.draw()
+        self.ice_station.draw()
+        self.whipped_cream.draw()
+        self.trash.draw()
+        if self.space_count > 3:
+          self.you.draw()
+
+        #MAKING DRINKS!!!
+        if self.drink == "Iced Coffee":
+            if arcade.check_for_collision(self.you,self.ice_station) and self.cup_empty == True:
+                self.cup = arcade.Sprite("game1_images/ice_cup.png",scale=0.18,center_x=400,center_y=305)
+                self.ice_done = True
+                self.cup_empty = False
+            if arcade.check_for_collision(self.you, self.coffee_station) and self.ice_done == True:
+                self.cup = arcade.Sprite("game1_images/ice_coffee1.png",scale=0.18,center_x=400,center_y=305)
+                self.coffee_done = True
+                self.ice_done = False
+            if arcade.check_for_collision(self.you, self.milk_station) and self.coffee_done == True:
+                self.cup = arcade.Sprite("game1_images/ice_coffee2.png",scale=0.18,center_x=400,center_y=305)
+                self.coffee_done = False
+                self.complete = True
+            self.check_complete()
+
+        elif self.drink == "Iced Tea":
+            if arcade.check_for_collision(self.you,self.ice_station) and self.cup_empty == True:
+                self.cup = arcade.Sprite("game1_images/ice_cup.png",scale=0.18,center_x=400,center_y=305)
+                self.ice_done = True
+                self.cup_empty = False
+            if arcade.check_for_collision(self.you,self.iced_tea) and self.ice_done == True:
+                #make a popup asking for selection of flavors, then set self.cup = respective flavor
+                self.cup = arcade.Sprite("game1_images/guava_white_tea.png",scale=0.18,center_x=400,center_y=305)
+                self.ice_done = False
+                self.complete = True
+                pass
+            self.check_complete()
+
+        elif self.drink == "Frappuccino":
+            if arcade.check_for_collision(self.you,self.syrup_station) and self.cup_empty == True:
+                self.cup = arcade.Sprite("game1_images/syrup.png", scale=0.18, center_x=400, center_y=305)
+                self.syrup_done = True
+                self.cup_empty = False
+            if arcade.check_for_collision(self.you,self.ice_station) and self.syrup_done == True:
+                self.cup = arcade.Sprite("game1_images/syrup_ice.png", scale=0.18, center_x=400, center_y=305)
+                self.ice_done = True
+                self.syrup_done = False
+            if arcade.check_for_collision(self.you,self.coffee_station) and self.ice_done == True:
+                self.cup = arcade.Sprite("game1_images/frap_ice.png",scale=0.18,center_x=400,center_y=305)
+                self.coffee_done = True
+                self.ice_done = False
+            if arcade.check_for_collision(self.you,self.whipped_cream) and self.coffee_done == True:
+                self.cup = arcade.Sprite("game1_images/frap_cream.png",scale=0.18,center_x=400,center_y=305)
+                self.coffee_done = False
+                self.complete = True
+            self.check_complete()
+
+        #trash can to clear
+        if arcade.check_for_collision(self.you,self.trash):
+            #start fresh
+            self.syrup_done = False
+            self.ice_done = False
+            self.coffee_done = False
+            self.complete = False
+            self.cup_empty = True
+            self.cup = arcade.Sprite("game1_images/cup.png", scale=0.18, center_x=400, center_y=305)
+
+
+        #show level
+        arcade.draw_text(f"Level: {self.level}",500,100,arcade.color.BLACK,anchor_x="center",
+                         anchor_y="center",align="center",font_name="Comic Sans MS",font_size=15)
+
+        #end failure popup
+        if self.failed == True:
+            arcade.draw_rectangle_filled(400,300,200,100,arcade.color.BANANA_MANIA)
+            arcade.draw_text("You failed!",400,300,arcade.color.BLACK,20,anchor_x="center",
+                             anchor_y="center",align="center")
+
+    def on_update(self, delta_time):
+        self.you.update()
+        if self.game_start == True:
+            if self.time > 0:
+                self.time -= delta_time
+            self.order = ["Iced Coffee!\nYou need: ice, coffee, and milk.",
+                      "Frappuccino!\nYou need: syrup, ice, espresso, and whipped cream.",
+                      "Matcha Iced Tea!\nYou need: ice and a matcha flavor shot.",
+                      "Passion Tango Iced Tea!\nYou need: ice and a passion tango flavor shot.",
+                      "Guava White Iced Tea Lemonade!\nYou need: ice and a guava white flavor shot."]
+
+            self.instructions = f"Order {self.order_num}: {self.order[(self.order_num) // len(self.order)]}"
+            self.drink_check()
+
+            if self.time < 0:
+                self.failed = True
+
+    def check_complete(self):
+        if arcade.check_for_collision(self.you, self.boss) and self.complete == True:
+            self.levels_completed += 1
+            self.complete = False
+            self.order_num += 1
+            self.cup = arcade.Sprite("game1_images/cup.png", scale=0.18, center_x=400, center_y=305)
+            self.cup_empty = True
+            if self.levels_completed <= 2:
+                self.time = 35
+                self.level = 1
+            elif self.levels_completed > 2 and self.levels_completed <= 5:
+                self.time = 20
+                self.level = 2
+            elif self.levels_completed > 5 and self.levels_completed <= 10:
+                self.time = 15
+                self.level = 3
+            elif self.levels_completed > 10:
+                self.time = 10
+                self.level = 4
+
+
+    def drink_check(self):
+        if "Iced Coffee" in self.instructions:
+            self.drink = "Iced Coffee"
+        elif "Iced Tea" in self.instructions:
+            self.drink = "Iced Tea"
+        elif "Frappuccino" in self.instructions:
+            self.drink = "Frappuccino"
 
     def on_key_press(self, key, modifiers):
-        arcade.start_render()
         if key == arcade.key.SPACE and self.space_count == 0:
-            self.cup.draw()
             self.press_space = ""
             self.instructions = "I'll handle the customer transactions and tell\n" \
                                 "you what drinks to make. This cup icon will\n" \
                                 "indicate what ingredients you've added."
             self.space_count += 1
         elif key == arcade.key.SPACE and self.space_count == 1:
-            self.instructions = "If you've messed up, you can start over by\n" \
+            self.instructions = "If you mess up, you can start over by\n" \
                                 "using the trash can. Be cognizant of how\n" \
                                 "long you take!"
             self.space_count += 1
         elif key == arcade.key.SPACE and self.space_count == 2:
             self.instructions = "...It's finals week, so students won't like\n" \
-                                "waiting too long. Ready? Good luck!"
+                                "to wait too long. Ready? Good luck!"
             self.space_count += 1
         elif key == arcade.key.SPACE and self.space_count == 3:
-            self.instructions = "Game starts..."
+            self.game_start = True
+            self.space_count += 1
+
+
+        #control player movement
+        if self.space_count > 0:
+            if key == arcade.key.LEFT:
+                self.you.change_x = -SPEED
+            elif key == arcade.key.RIGHT:
+                self.you.change_x = +SPEED
+            elif key == arcade.key.DOWN:
+                self.you.change_y = -SPEED
+            elif key == arcade.key.UP:
+                self.you.change_y = +SPEED
+
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.LEFT:
+            self.you.change_x = 0
+        elif key == arcade.key.RIGHT:
+            self.you.change_x = 0
+        elif key == arcade.key.DOWN:
+            self.you.change_y = 0
+        elif key == arcade.key.UP:
+            self.you.change_y = 0
 
 
 def main():
         """ Main method """
 
+        """ This is the actual game main() """
         window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         start_view = InstructionView()
         window.show_view(start_view)
         arcade.run()
 
+        """ This is the original game main() that I wanted to save. """
         # game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         # # arcade.set_background_color(arcade.color.ANTIQUE_WHITE)
         # # arcade.start_render()
